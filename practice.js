7a28a875df0d9eeddb753203d62586414f9d6da8
Nicholas Mastracola <nicholas.mastracola@gmail.com>
2016-12-05 13:11:15 -0700

    finished problem 3

--- practice.js
+++ practice.js
@@ -56,17 +56,20 @@ Set the value of that key to 'chapstick'.
 Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
-  //Code here
+  var backPack = {},
+      item = "firstPocket";
+  backPack[item] = "chapstick";
+  backPack.color = "grey";
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+  alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-  //Code here
+  console.log(backPack);
 
 
 
